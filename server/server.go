df9b98fef5612277f864d180c80c99c9fd7001e2
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-02-11 23:41:02 +0000

    feat: server routing

--- server/server.go
+++ server/server.go
@@ -1,10 +1,13 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/raphael-p/beango/resolvers"
@@ -35,26 +38,60 @@ func (w *loggingResponseWriter) String() string {
 	return out
 }
 
-func logger(h func(http.ResponseWriter, *http.Request)) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestString := fmt.Sprint(r.Method, " ", r.URL)
-		fmt.Println("received ", requestString)
-		loggingResponseWriter := &loggingResponseWriter{ResponseWriter: w}
-		start := time.Now()
-		http.HandlerFunc(h).ServeHTTP(loggingResponseWriter, r)
-		loggingResponseWriter.time = time.Since(start).Milliseconds()
-		fmt.Printf(
-			"%s resolved with %s\n",
-			requestString,
-			loggingResponseWriter,
-		)
-	})
+func logger(h http.HandlerFunc, w http.ResponseWriter, r *http.Request) {
+	requestString := fmt.Sprint(r.Method, " ", r.URL)
+	fmt.Println("received ", requestString)
+	loggingResponseWriter := &loggingResponseWriter{ResponseWriter: w}
+	start := time.Now()
+	h(loggingResponseWriter, r)
+	loggingResponseWriter.time = time.Since(start).Milliseconds()
+	fmt.Printf(
+		"%s resolved with %s\n",
+		requestString,
+		loggingResponseWriter,
+	)
+}
 
+type route struct {
+	method  string
+	pattern *regexp.Regexp
+	handler http.HandlerFunc
+}
+
+var routes []route
+
+type ctxKey struct{}
+
+func newRoute(method, pattern string, handler http.HandlerFunc) {
+	route := route{method, regexp.MustCompile("^" + pattern + "$"), handler}
+	routes = append(routes, route)
+}
+
+func router(w http.ResponseWriter, r *http.Request) {
+	var allow []string
+	for _, route := range routes {
+		matches := route.pattern.FindStringSubmatch(r.URL.Path)
+		if len(matches) > 0 {
+			if r.Method != route.method {
+				allow = append(allow, route.method)
+				continue
+			}
+			ctx := context.WithValue(r.Context(), ctxKey{}, matches[1:])
+			logger(route.handler, w, r.WithContext(ctx))
+			return
+		}
+	}
+	if len(allow) > 0 {
+		w.Header().Set("Allow", strings.Join(allow, ", "))
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	http.NotFound(w, r)
 }
 
 func Start() {
-	http.Handle("/users", logger(resolvers.GetUsers))
-	http.Handle("/user", logger(resolvers.CreateUser))
+	newRoute(http.MethodGet, "/users", resolvers.GetUsers)
+	newRoute(http.MethodPost, "/user", resolvers.CreateUser)
 	// "/chats", resolvers.GetChats
 	// "/chat", resolvers.CreateChat
 	// "/messages", resolvers.GetMessages
@@ -65,8 +102,8 @@ func Start() {
 		fmt.Printf("error starting server: %s\n", err)
 	}
 	fmt.Println("🐱‍💻 BeanGo server started on", l.Addr().String())
-	if err := http.Serve(l, nil); err != nil {
-		fmt.Println("server closed")
+	if err := http.Serve(l, http.HandlerFunc(router)); err != nil {
+		fmt.Printf("server closed: %s\n", err)
 	}
 	os.Exit(1)
 }
