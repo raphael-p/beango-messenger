b191ee12f5b7f9f8b6166a6b798caad3e18d6b68
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-02-17 22:17:16 +0000

    feat: path parameters now supported + refactor

--- server/router.go
+++ server/router.go
@@ -14,49 +14,58 @@ import (
 type handlerFunc func(*utils.ResponseWriter, *http.Request)
 
 type route struct {
-	method  string
-	pattern *regexp.Regexp
-	handler handlerFunc
+	method       string
+	pattern      *regexp.Regexp
+	innerHandler handlerFunc
+	paramKeys    []string
 }
 
 type router struct {
-	routes []route
+	routes []*route
 }
 
 func newRouter() *router {
-	return &router{routes: []route{}}
+	return &router{routes: []*route{}}
 }
 
-func (r *router) newRoute(method, pattern string, handler handlerFunc) {
-	route := route{method, regexp.MustCompile("^" + pattern + "$"), handler}
-	r.routes = append(r.routes, route)
+func (r *router) addRoute(method, endpoint string, handler handlerFunc) {
+	// handle path parameters
+	pathParamPattern := regexp.MustCompile(":([a-z]+)")
+	matches := pathParamPattern.FindAllStringSubmatch(endpoint, -1)
+	paramKeys := []string{}
+	if len(matches) > 0 {
+		// replace path parameter definition with regex pattern to capture any string
+		endpoint = pathParamPattern.ReplaceAllLiteralString(endpoint, "([^/]+)")
+		// store the names of path parameters, to later be used as context keys
+		for i := 0; i < len(matches); i++ {
+			paramKeys = append(paramKeys, matches[i][1])
+		}
+	}
+	route := route{method, regexp.MustCompile("^" + endpoint + "$"), handler, paramKeys}
+	r.routes = append(r.routes, &route)
 }
 
 func (r *router) GET(pattern string, handler handlerFunc) {
-	r.newRoute(http.MethodGet, pattern, handler)
+	r.addRoute(http.MethodGet, pattern, handler)
 }
 
 func (r *router) POST(pattern string, handler handlerFunc) {
-	r.newRoute(http.MethodPost, pattern, handler)
+	r.addRoute(http.MethodPost, pattern, handler)
 }
 
 func (r *router) PUT(pattern string, handler handlerFunc) {
-	r.newRoute(http.MethodPut, pattern, handler)
+	r.addRoute(http.MethodPut, pattern, handler)
 }
 
 func (r *router) PATCH(pattern string, handler handlerFunc) {
-	r.newRoute(http.MethodPatch, pattern, handler)
+	r.addRoute(http.MethodPatch, pattern, handler)
 }
 
 func (r *router) DELETE(pattern string, handler handlerFunc) {
-	r.newRoute(http.MethodDelete, pattern, handler)
+	r.addRoute(http.MethodDelete, pattern, handler)
 }
 
 func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	r.serveHTTP(utils.NewResponseWriter(w), req)
-}
-
-func (r *router) serveHTTP(w *utils.ResponseWriter, req *http.Request) {
 	var allow []string
 	for _, route := range r.routes {
 		matches := route.pattern.FindStringSubmatch(req.URL.Path)
@@ -65,8 +74,10 @@ func (r *router) serveHTTP(w *utils.ResponseWriter, req *http.Request) {
 				allow = append(allow, route.method)
 				continue
 			}
-			ctx := context.WithValue(req.Context(), struct{}{}, matches[1:])
-			logger(route.handler, w, req.WithContext(ctx))
+			route.handler(
+				utils.NewResponseWriter(w),
+				buildContext(req, route.paramKeys, matches[1:]),
+			)
 			return
 		}
 	}
@@ -78,11 +89,25 @@ func (r *router) serveHTTP(w *utils.ResponseWriter, req *http.Request) {
 	http.NotFound(w, req)
 }
 
-func logger(h handlerFunc, w *utils.ResponseWriter, r *http.Request) {
-	requestString := fmt.Sprint(r.Method, " ", r.URL)
+// Server's context key, used to avoid clashes
+type ContextKey string
+
+// Returns a shallow-copy of the request with an updated context,
+// including path parameters
+func buildContext(req *http.Request, paramKeys, paramValues []string) *http.Request {
+	ctx := req.Context()
+	for i := 0; i < len(paramKeys); i++ {
+		ctx = context.WithValue(ctx, ContextKey(paramKeys[i]), paramValues[i])
+	}
+	return req.WithContext(ctx)
+}
+
+// A wrapper around a route's handler, used for logging
+func (r *route) handler(w *utils.ResponseWriter, req *http.Request) {
+	requestString := fmt.Sprint(req.Method, " ", req.URL)
 	fmt.Println("received ", requestString)
 	start := time.Now()
-	h(w, r)
+	r.innerHandler(w, req)
 	w.Time = time.Since(start).Milliseconds()
 	fmt.Printf("%s resolved with %s\n", requestString, w)
 }
