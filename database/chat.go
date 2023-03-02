05f4a14ef4b93c1cee9a94495b75cf81a2b85766
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-03-02 22:40:16 +0000

    feat: GetChat function for database

--- database/chat.go
+++ database/chat.go
@@ -1,8 +1,21 @@
 package database
 
+import (
+	"fmt"
+)
+
 type Chat struct {
 	Id      string    `json:"id"`
 	UserIds [2]string `json:"userIds"`
+}
+
+func GetChat(id string) (*Chat, error) {
+	chat, ok := Chats[id]
+	if !ok {
+		return nil, fmt.Errorf("no chat found with id %s", id)
+	} else {
+		return &chat, nil
+	}
 }
 
 func GetChatsByUserId(userId string) []Chat {
