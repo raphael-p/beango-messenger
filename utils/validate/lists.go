27cd6619848cf8ff75a98032019d618e0d97676c
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-04-08 21:33:42 +0100

    fix: flipped bool in UniqueList

--- utils/validate/lists.go
+++ utils/validate/lists.go
@@ -3,7 +3,7 @@ package validate
 func UniqueList[K comparable](s []K) bool {
 	seen := make(map[K]bool)
 	for _, str := range s {
-		if _, ok := seen[str]; !ok {
+		if _, ok := seen[str]; ok {
 			return false
 		}
 		seen[str] = true
