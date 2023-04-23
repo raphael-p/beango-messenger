5cdbb6b526335fa4111b5177507456ff3619973d
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-04-23 21:56:36 +0100

    test: nicer assert messages

--- test/assert/assert.go
+++ test/assert/assert.go
@@ -7,19 +7,19 @@ import (
 
 func HasLength[T any](t *testing.T, list []T, expectedLength int) {
 	if length := len(list); length != expectedLength {
-		t.Errorf("expected list to be of length %d, but got %d", expectedLength, length)
+		t.Errorf("expected list to be of length %d, got %d", expectedLength, length)
 	}
 }
 
 func IsNil(t *testing.T, expectedValue any) {
 	if expectedValue != nil {
-		t.Errorf("expected nil, but got \"%v\"", expectedValue)
+		t.Errorf("expected nil, got \"%v\"", expectedValue)
 	}
 }
 
 func Equals[T comparable](t *testing.T, value T, expectedValue T) {
 	if value != expectedValue {
-		t.Errorf("expected \"%v\", but got \"%v\"", expectedValue, value)
+		t.Errorf("expected \"%v\", got \"%v\"", expectedValue, value)
 	}
 }
 
@@ -30,19 +30,19 @@ func DeepEquals(t *testing.T, value any, expectedValue any) {
 			reflectType = reflectType.Elem()
 		}
 		if typeName := reflectType.Name(); typeName != "" {
-			t.Errorf("expected %s %v, but got %v", typeName, expectedValue, value)
+			t.Errorf("expected %s \"%v\", got \"%v\"", typeName, expectedValue, value)
 			return
 		}
-		t.Errorf("expected %v, but got %v", expectedValue, value)
+		t.Errorf("expected \"%v\", got \"%v\"", expectedValue, value)
 	}
 }
 
 func ErrorHasMessage(t *testing.T, err error, expectedMessage string) {
 	if err == nil {
-		t.Error("expected error, but got nil")
+		t.Error("expected error, got nil")
 		return
 	}
 	if err.Error() != expectedMessage {
-		t.Errorf("expected error %v, but got %v", err.Error(), expectedMessage)
+		t.Errorf("expected error \"%v\", got \"%v\"", err.Error(), expectedMessage)
 	}
 }
