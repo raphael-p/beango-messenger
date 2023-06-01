03a069ebf79b88cbddc7c13bf07b0d2da18295cd
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-06-01 22:37:39 +0100

    test: cleanup mock loggers without copying mutex

--- utils/logger/mocks.go
+++ utils/logger/mocks.go
@@ -2,25 +2,24 @@ package logger
 
 import (
 	"bytes"
-	"log"
 	"testing"
 )
 
 func MockConsoleLogger(t *testing.T) *bytes.Buffer {
-	oldStdOutLogger := *Logger.StdOutLogger
-	t.Cleanup(func() { Logger.StdOutLogger = &oldStdOutLogger })
+	oldWriter := Logger.StdOutLogger.Writer()
+	t.Cleanup(func() { Logger.StdOutLogger = NewLogger(oldWriter) })
 	var buf bytes.Buffer
 	Logger.StdOutLogger = NewLogger(&buf)
 	return &buf
 }
 
 func MockFileLogger(t *testing.T) *bytes.Buffer {
-	var oldFileLogger *log.Logger
 	if Logger.FileLogger != nil {
-		loggerCopy := *Logger.FileLogger
-		oldFileLogger = &loggerCopy
+		oldWriter := Logger.FileLogger.Writer()
+		t.Cleanup(func() { Logger.FileLogger = NewLogger(oldWriter) })
+	} else {
+		t.Cleanup(func() { Logger.FileLogger = nil })
 	}
-	t.Cleanup(func() { Logger.FileLogger = oldFileLogger })
 	var buf bytes.Buffer
 	Logger.FileLogger = NewLogger(&buf)
 	return &buf
