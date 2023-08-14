71067b5a11f95496c370ef6721351677b6e525ca
Raphaël Piccolin <raphael.piccolin@ripjar.com>
2023-08-14 12:51:14 +0200

    refactor: removed cloneRequest

--- client/utils.go
+++ client/utils.go
@@ -1,9 +1,7 @@
 package client
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/raphael-p/beango/utils/response"
@@ -16,14 +14,4 @@ func DisplayError(w *response.Writer, message string) {
 	}
 	htmlStr := fmt.Sprintf("<div id='errors' class='error' hx-swap-oob='innerHTML'>%s</div>", message)
 	w.WriteString(http.StatusOK, htmlStr)
-}
-
-// TODO: unit test
-func cloneRequest(r *http.Request) *http.Request {
-	clone := r.Clone(r.Context())
-	// TODO: investigate error handling: can ignoring this error cause a panic?
-	if body, err := io.ReadAll(r.Body); err == nil {
-		clone.Body = io.NopCloser(bytes.NewReader(body))
-	}
-	return clone
 }
