88ff7ed6f8895e5de6e698a57de3b75645910edf
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-10-05 21:51:04 +0100

    chore: temporarily using fixed htmx

--- client/skeleton.go
+++ client/skeleton.go
@@ -4,7 +4,6 @@ import "html/template"
 
 var skeleton *template.Template
 
-// TODO: use local htmx.min & json-enc instead of having to download from unpkg
 func GetSkeleton() (*template.Template, error) {
 	if skeleton != nil {
 		return skeleton, nil
@@ -12,8 +11,8 @@ func GetSkeleton() (*template.Template, error) {
 	tmpl := `<!DOCTYPE html>
 		<html>
 		<head>
-			<script src="https://unpkg.com/htmx.org@1.9.6"></script>
-			<script src="https://unpkg.com/htmx.org/dist/ext/json-enc.js"></script>
+			<script src="/resources/htmx.js"></script>
+			<script src="/resources/json-enc.js"></script>
 			<script src="/resources/script.js"></script>
 			<link rel="stylesheet" type="text/css" href="/resources/style.css">
 			<title>Beango Messenger</title>
