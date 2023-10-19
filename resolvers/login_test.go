001ca08459e951735a4700f8f8b2fb9c056d259a
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-10-19 17:37:58 +0100

    test: SubmitLogin

--- resolvers/login_test.go
+++ resolvers/login_test.go
@@ -1,13 +1,17 @@
 package resolvers
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/raphael-p/beango/config"
+	"github.com/raphael-p/beango/database"
 	"github.com/raphael-p/beango/resolvers/resolverutils"
 	"github.com/raphael-p/beango/test/assert"
 	"github.com/raphael-p/beango/test/mocks"
 	"github.com/raphael-p/beango/utils/cookies"
+	"github.com/raphael-p/beango/utils/response"
 )
 
 func TestLogin(t *testing.T) {
@@ -39,5 +43,38 @@ func TestLogin(t *testing.T) {
 		Login(w, req, conn)
 		assert.Equals(t, w.Status, http.StatusOK)
 		assert.Contains(t, string(w.Body), "<html>", "</html")
+	})
+}
+
+func TestSubmitLogin(t *testing.T) {
+	config.CreateConfig()
+
+	body := func(username, password string) string {
+		return fmt.Sprintf(`{"username": "%s", "password": "%s"}`, username, password)
+	}
+
+	checkSuccessfulLogin := func(w *response.Writer, req *http.Request, conn database.Connection) {
+		SubmitLogin(w, req, conn)
+		assert.Equals(t, w.Status, http.StatusNoContent)
+		assert.Equals(t, string(w.Body), "")
+		assert.Equals(t, w.Header().Get("HX-Redirect"), "/home")
+	}
+
+	t.Run("NormalWithLogin", func(t *testing.T) {
+		mockUser := mocks.MakeUser()
+		w, req, conn := resolverutils.CommonSetup(body(mockUser.Username, mocks.PASSWORD))
+		conn.SetUser(mockUser)
+		param := map[string]string{"action": "login"}
+		req = resolverutils.SetContext(t, req, mocks.Admin, param)
+
+		checkSuccessfulLogin(w, req, conn)
+	})
+
+	t.Run("NormalWithSignup", func(t *testing.T) {
+		w, req, conn := resolverutils.CommonSetup(body("someNewUser", "123"))
+		param := map[string]string{"action": "signup"}
+		req = resolverutils.SetContext(t, req, mocks.Admin, param)
+
+		checkSuccessfulLogin(w, req, conn)
 	})
 }
