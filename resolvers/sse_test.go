b4830e199eb1149c23ff295c56e0d400ba03709e
Raphael Piccolin <raphael.piccolin@outlook.com>
2023-12-26 18:42:45 +0000

    test: SendEvent

--- resolvers/sse_test.go
+++ resolvers/sse_test.go
@@ -1,12 +1,15 @@
 package resolvers
 
 import (
+	"fmt"
+	"net/http"
 	"net/http/httptest"
 	"sort"
 	"testing"
 
 	"github.com/raphael-p/beango/test/assert"
 	"github.com/raphael-p/beango/utils/collections"
+	"github.com/raphael-p/beango/utils/logger"
 	"github.com/raphael-p/beango/utils/response"
 )
 
@@ -15,7 +18,7 @@ func TestRegisterConnection(t *testing.T) {
 		var key int64 = 1
 		xStatus := 1
 		xConnectionIndex := connectionIndex{}
-		w := *response.NewWriter(httptest.NewRecorder())
+		w := response.NewWriter(httptest.NewRecorder())
 
 		registerConnection(w, xConnectionIndex, key)
 		_, values := collections.MapEntries(xConnectionIndex[key])
@@ -25,7 +28,7 @@ func TestRegisterConnection(t *testing.T) {
 
 	t.Run("MultipleConnectionsOnKey", func(t *testing.T) {
 		var key int64 = 1
-		w := *response.NewWriter(httptest.NewRecorder())
+		w := response.NewWriter(httptest.NewRecorder())
 		xConnectionIndex := connectionIndex{}
 
 		registerConnection(w, xConnectionIndex, key)
@@ -37,7 +40,7 @@ func TestRegisterConnection(t *testing.T) {
 
 func TestCloseSSEConnection(t *testing.T) {
 	setup := func() connectionIndex {
-		w := *response.NewWriter(httptest.NewRecorder())
+		w := response.NewWriter(httptest.NewRecorder())
 		return connectionIndex{
 			0: {"a": w},
 			1: {"a": w, "b": w, "c": w},
@@ -69,5 +72,35 @@ func TestCloseSSEConnection(t *testing.T) {
 		assert.HasLength(t, keys, 3)
 		_, ok := xConnectionIndex[emptyKey]
 		assert.Equals(t, ok, false)
+	})
+}
+
+func TestSendEvent(t *testing.T) {
+	t.Run("Normal", func(t *testing.T) {
+		w := response.NewWriter(httptest.NewRecorder())
+		connections := connectionMap{"a": w}
+		buf := logger.MockFileLogger(t)
+		xEvent := "test-event"
+		xData := "Hello World!"
+		xMessage := fmt.Sprintf("[SSE connection a] sent '%s' event", xEvent)
+
+		sendEvent(connections, xEvent, xData)
+		assert.Equals(t, w.Status, http.StatusOK)
+		assert.Contains(t, string(w.Body), xEvent, xData)
+		assert.Contains(t, buf.String(), xMessage)
+	})
+
+	t.Run("SendsMultiple", func(t *testing.T) {
+		w1 := response.NewWriter(httptest.NewRecorder())
+		w2 := response.NewWriter(httptest.NewRecorder())
+		connections := connectionMap{"a": w1, "b": w2}
+		xEvent := "test-event"
+		xData := "Hello World!"
+
+		sendEvent(connections, xEvent, xData)
+		assert.Equals(t, w1.Status, http.StatusOK)
+		assert.Contains(t, string(w1.Body), xEvent, xData)
+		assert.Equals(t, w2.Status, http.StatusOK)
+		assert.Contains(t, string(w2.Body), xEvent, xData)
 	})
 }
